218eb6124975a53ef259818eafba9134fd8911a5
Adam Staněk <adam.stanek@v3net.cz>
2016-03-17 14:40:19 +0100

    search example: added paging

--- examples/search.js
+++ examples/search.js
@@ -3,7 +3,11 @@ const { Router, Route, ParamTypes } = require('../index.js');
 
 describe('Search keywords example', function () {
   var route = (
-    <Route path="search[/:keywords]" name="r1" params={{ keywords: ParamTypes.keywords }} />
+    <Route
+      path="search[/:keywords[/:pageNum]]"
+      params={{ keywords: ParamTypes.keywords, pageNum: ParamTypes.integer.gt(0).withDefault(1) }}
+      name="r1"
+    />
   );
 
   describe('URL matching', function () {
@@ -16,16 +20,18 @@ describe('Search keywords example', function () {
       });
     }
 
-    itMatches('search');
-    itMatches('search/');
-    itMatches('search/foo-bar', { keywords: 'foo bar' });
-    itMatches('search/foo-bar/', { keywords: 'foo bar' });
+    itMatches('search', { pageNum: 1 });
+    itMatches('search/', { pageNum: 1 });
+    itMatches('search/foo-bar', { keywords: 'foo bar', pageNum: 1 });
+    itMatches('search/foo-bar/', { keywords: 'foo bar', pageNum: 1 });
+    itMatches('search/foo-bar/2', { keywords: 'foo bar', pageNum: 2 });
+    itMatches('search/foo-bar/2/', { keywords: 'foo bar', pageNum: 2 });
   });
 
   describe('URL construction', function () {
     var router = new Router([route]);
 
-    it('generates URL without specified keywords', function () {
+    it.only('generates URL without specified keywords', function () {
       var url = router.createUrl('r1');
       expect(url).to.be.equal('/search');
     });
@@ -33,6 +39,11 @@ describe('Search keywords example', function () {
     it('generates URL for search keywords "foo bar"', function () {
       var url = router.createUrl('r1', { keywords: "foo bar" });
       expect(url).to.be.equal('/search/foo-bar');
+    });
+
+    it('generates URL for search keywords "foo bar", 2nd page', function () {
+      var url = router.createUrl('r1', { keywords: "foo bar", pageNum: 2 });
+      expect(url).to.be.equal('/search/foo-bar/2');
     });
   });
 });
