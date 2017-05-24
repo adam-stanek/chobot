b42cceee644a54d11bb3979379a1a98981095944
Adam Staněk <adam.stanek@v3net.cz>
2017-05-24 12:12:26 +0200

    exporting routeWalk

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ module.exports = {
   Router: require('./src/Router.js'),
   Route: Route,
   ParamTypes: require('./src/param-types/index.js'),
-  ensureRoute: require('./src/utils/ensureRoute.js').bind(null, Route)
+  ensureRoute: require('./src/utils/ensureRoute.js').bind(null, Route),
+  routeWalk: require('./src/utils/routeWalk.js')
 };
